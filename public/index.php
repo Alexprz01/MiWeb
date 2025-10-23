50c2bcfbd55ec445fd0c2f1ac20a04ed42db11ad
alex perez <adaw23aperez@inslaferreria.cat>
2025-10-23 17:14:02 +0200

    index.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="ca">
 
 <head>
     <meta charset="UTF-8">
@@ -11,20 +11,25 @@
     <h1><?php echo "Hola món des de PHP i Docker!"; ?></h1>
 
     <?php
+    // Dades de connexió
     $servername = "db";
     $username = "user";
     $password = "password";
     $database = "demo";
 
+    // Crear connexió
     $conn = new mysqli($servername, $username, $password, $database);
 
+    // Comprovar connexió
     if ($conn->connect_error) {
-        die("❌ Connexió fallida: " . $conn->connect_error);
+        die("❌ Connexió fallida: " . htmlspecialchars($conn->connect_error));
     }
-    echo "✅ Connexió correcta a la base de dades MySQL!";
+
+    echo "<p>✅ Connexió correcta a la base de dades MySQL!</p>";
+
+    // Tancar connexió
     $conn->close();
     ?>
-
 </body>
 
 </html>
