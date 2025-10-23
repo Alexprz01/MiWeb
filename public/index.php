4ca0ec5abb7e9457aa1102895094fd6d9d762aae
alex perez <adaw23aperez@inslaferreria.cat>
2025-10-23 17:28:56 +0200

    arreglo deploy.yml

--- public/index.php
+++ public/index.php
@@ -9,24 +9,19 @@
 
 <body>
     <h1><?php echo "Hola món des de PHP i Docker!"; ?></h1>
-
     <?php
     // Dades de connexió
     $servername = "db";
     $username = "user";
     $password = "password";
     $database = "demo";
-
     // Crear connexió
     $conn = new mysqli($servername, $username, $password, $database);
-
     // Comprovar connexió
     if ($conn->connect_error) {
-        die("❌ Connexió fallida: " . htmlspecialchars($conn->connect_error));
+        die(" Connexió fallida: " . htmlspecialchars($conn->connect_error));
     }
-
-    echo "<p>✅ Connexió correcta a la base de dades MySQL!</p>";
-
+    echo "<p> Connexió correcta a la base de dades MySQL!</p>";
     // Tancar connexió
     $conn->close();
     ?>
