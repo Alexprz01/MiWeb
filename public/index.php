1def0a7f9a37bc4e90dbee213e02c075e47b6398
alex perez <adaw23aperez@inslaferreria.cat>
2025-10-23 15:56:40 +0200

    index.php

--- public/index.php
+++ public/index.php
@@ -1,2 +1,15 @@
 <?php
-echo "<h1>Hola Mön 👋</h1>";
+echo "<h1>Hola Mön </h1>";
+
+$host = 'db';
+$db   = 'demo';
+$user = 'user';
+$pass = 'password';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "<h2> Connexió correcta a la base de dades!</h2>";
+} catch (PDOException $e) {
+    echo "<h2> Error de connexió: " . $e->getMessage() . "</h2>";
+}
