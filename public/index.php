8a28c788f739120e1fcba81a099fa5a7709d1492
alex perez <adaw23aperez@inslaferreria.cat>
2025-10-24 18:51:01 +0200

    cambios pro problemas conectar bd

--- public/index.php
+++ public/index.php
@@ -10,20 +10,16 @@
 <body>
     <h1><?php echo "Hola món des de PHP i Docker!"; ?></h1>
     <?php
-    // Dades de connexió
     $servername = "db";
     $username = "user";
     $password = "password";
     $database = "demo";
-    // Crear connexió
+
     try {
-        // Crear connexió PDO
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+        $conn = new PDO("mysql:host=$servername;dbname=$database;charset=utf8", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        echo "<p class='ok'>✅ Connexió establerta correctament amb la base de dades <strong>$dbname</strong>.</p>";
-
-        // Exemple de consulta
+        echo "<p class='ok'>✅ Connexió establerta correctament amb la base de dades <strong>$database</strong>.</p>";
         $stmt = $conn->query("SELECT NOW() AS hora_actual");
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
